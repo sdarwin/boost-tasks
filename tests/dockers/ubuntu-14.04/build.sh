36c80bff5ba928ada36338875627f7476ff012e5
sdarwin <samuel.d.darwin@gmail.com>
2021-06-29 23:10:20 +0000

    Dockerfile test environment: include jfrog artifactory

--- tests/dockers/ubuntu-14.04/build.sh
+++ tests/dockers/ubuntu-14.04/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ -z "$GITHUBTOKEN" ]; then
+  echo "Please set the github token first."
+  echo "export GITHUBTOKEN=__"
+  exit 1
+fi
+
 docker build --build-arg GITHUBTOKEN=$GITHUBTOKEN .
